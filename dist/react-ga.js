33db372ef9a0bb7f12a530a405931c567728ffbd
SimeonC <simeon@kkvesper.jp>
2017-09-26 09:58:07 +0900

    fix(Webpack): Add the correct build process to fix exports

--- dist/react-ga.js
+++ dist/react-ga.js
@@ -1,3 +1,4 @@
+module.exports =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
---
+++ version-bower.js
@@ -0,0 +1,8 @@
+const fs = require('fs-extra');
+
+const mainPackage = require('./package.json');
+const bower = require('./bower.json');
+
+bower.version = mainPackage.version;
+
+fs.writeJsonSync('./bower.json', bower, { spaces: 2 });
