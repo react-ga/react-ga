2eb5dfaee55aa3c289673d5a3abaf64a6ccc2fa2
SimeonC <simeon@kkvesper.jp>
2017-11-04 17:02:20 +0900

    Add a example setup for working app.

---
+++ demo/app/index.jsx
@@ -0,0 +1,141 @@
+import React, { PureComponent } from 'react';
+import ReactGA from '../../src';
+
+import Router from './Router';
+
+function asDisplayString(value) {
+  if (typeof value === 'string') return `'${value}'`;
+  return value.toString();
+}
+
+function renderConfigString(config, indent = '') {
+  return [
+    '{',
+    ...Object.keys(config).map((key) => {
+      if (config[key] === undefined || config[key] === null) return '';
+      return `\t${key}: ${asDisplayString(config[key])}`;
+    }).filter(value => value),
+    '}'
+  ].reduce(
+    (result, element, index, array) => {
+      return `${result}${indent}${element}${index < array.length - 1 ? '\n' : ''}`;
+    },
+    ''
+  );
+}
+
+const DEFAULT_CONFIG = {
+  trackingId: '',
+  debug: true
+};
+
+export default class App extends PureComponent {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      reactGaInitialised: false,
+      configs: [DEFAULT_CONFIG]
+    };
+  }
+
+  filteredConfigs = () => {
+    return this.state.configs.filter(({ trackingId: id }) => id);
+  };
+
+  initReactGA = () => {
+    if (this.filteredConfigs().length === 0) {
+      return;
+    }
+    ReactGA.initialize(this.state.configs);
+    // Send initial test view
+    ReactGA.pageview('test-init-pageview');
+    this.setState({ reactGaInitialised: true });
+  };
+
+  addConfig = () => {
+    this.setState({
+      configs: [...this.state.configs, DEFAULT_CONFIG]
+    });
+  };
+
+  updateConfig = (configIndex, key, type, event) => {
+    const config = this.state.configs[configIndex];
+    let value;
+    if (type === 'checkbox') {
+      value = !config[key];
+    } else {
+      value = event.target.value;
+    }
+    const newConfig = {
+      ...config,
+      [key]: value
+    };
+    this.setState({
+      configs: [
+        ...this.state.configs.slice(0, configIndex),
+        newConfig,
+        ...this.state.configs.slice(configIndex + 1)
+      ]
+    });
+  };
+
+  renderConfigs = () => {
+    const configs = this.filteredConfigs();
+    if (configs.length === 0) return '';
+    if (configs.length === 1) {
+      const { trackingId, ...options } = configs[0];
+      return `'${trackingId}'${Object.values(options).filter(val => val).length ? `, ${JSON.stringify(options)}` : ''}`;
+    }
+    return `[\n${
+      configs.reduce((result, config, index, array) => {
+        const configString = renderConfigString(config, '\t');
+        return `${result}${result ? '\n' : ''}${configString}${index < array.length - 1 ? ',' : ''}`;
+      }, '')
+    }\n]`;
+  };
+
+  render() {
+    const { configs, reactGaInitialised } = this.state;
+    if (reactGaInitialised) {
+      return (
+        <div>
+          <h4>App is Initialised. Refresh page to reset.</h4>
+          <Router />
+        </div>
+      );
+    }
+    let initializationDebug = (
+      <pre>
+        ReactGA.initialize({this.renderConfigs()});
+      </pre>
+    );
+    if (this.filteredConfigs().length === 0) {
+      initializationDebug = <p>No Valid Configs set.</p>;
+    }
+    return (
+      <form onSubmit={this.initReactGA}>
+        <p>Input your configs below:</p>
+        {configs.map(({ trackingId, debug }, index) => (
+          <div key={index}>
+            <div>
+              TrackingID:&nbsp;
+              <input value={trackingId} onChange={this.updateConfig.bind(this, index, 'trackingId', 'text')} />
+              &nbsp;Debug&nbsp;
+              <input
+                type="checkbox"
+                checked={debug || false}
+                onChange={this.updateConfig.bind(this, index, 'debug', 'checkbox')}
+              />
+            </div>
+          </div>
+        ))}
+        <button onClick={this.addConfig}>Add Config</button>
+        <button type="submit" disabled={configs.length < 1}>
+          {configs.length < 1 ? 'Add Configs first' : 'Run the initialize function as below'}
+        </button>
+        {initializationDebug}
+      </form>
+    );
+  }
+}
---
+++ demo/app/withTracker.jsx
@@ -0,0 +1,38 @@
+/**
+ * From ReactGA Community Wiki Page https://github.com/react-ga/react-ga/wiki/React-Router-v4-withTracker
+ */
+
+import React, { Component } from 'react';
+import GoogleAnalytics from '../../src';
+
+export default function withTracker(WrappedComponent, options = {}) {
+  const trackPage = (page) => {
+    GoogleAnalytics.set({
+      page,
+      ...options
+    });
+    GoogleAnalytics.pageview(page);
+  };
+
+  const HOC = class extends Component {
+    componentDidMount() {
+      const page = this.props.location.pathname;
+      trackPage(page);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      const currentPage = this.props.location.pathname;
+      const nextPage = nextProps.location.pathname;
+
+      if (currentPage !== nextPage) {
+        trackPage(nextPage);
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+
+  return HOC;
+}
