564c8b938e9be7f86bd1c541c6a8267282c978f8
rahul <rahul@lightning-bolt.com>
2018-05-01 15:02:05 -0700

    Support import * as ReactGA for noreact refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import UnboundOutboundLink from './components/OutboundLink';
 import * as Defaults from './noreact';
 
+export const initialize = Defaults.initialize;
+export const ga = Defaults.ga;
+export const set = Defaults.set;
+export const send = Defaults.send;
+export const pageview = Defaults.pageview;
+export const modalview = Defaults.modalview;
+export const timing = Defaults.timing;
+export const event = Defaults.event;
+export const exception = Defaults.exception;
+export const plugin = Defaults.plugin;
+export const outboundLink = Defaults.outboundLink;
+export const testModeAPI = Defaults.testModeAPI;
+
 UnboundOutboundLink.origTrackLink = UnboundOutboundLink.trackLink;
 UnboundOutboundLink.trackLink = Defaults.outboundLink;
 export const OutboundLink = UnboundOutboundLink;
