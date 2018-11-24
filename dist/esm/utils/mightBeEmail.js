f9306099533112fc866b35d8443f1b1f49ccbaaf
Simeon Cheeseman <simeon@tablecheck.com>
2018-11-24 11:59:33 +0900

    2.5.4

---
+++ dist/esm/utils/mightBeEmail.js
@@ -0,0 +1,6 @@
+// See if s could be an email address. We don't want to send personal data like email.
+// https://support.google.com/analytics/answer/2795983?hl=en
+export default function mightBeEmail(s) {
+  // There's no point trying to validate rfc822 fully, just look for ...@...
+  return /[^@]+@[^@]+/.test(s);
+}
---
+++ dist/esm/utils/testModeAPI.js
@@ -0,0 +1,11 @@
+export var gaCalls = [];
+export default {
+  calls: gaCalls,
+  ga: function ga() {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    gaCalls.push(args.concat());
+  }
+};
