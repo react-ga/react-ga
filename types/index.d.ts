4f47ae140da1e9583954a49086f215938c10e128
lezi.chen <pleasurazy@gmail.com>
2019-12-03 08:42:21 +0800

    add options.redactEmail for reactGA.initialize

--- types/index.d.ts
+++ types/index.d.ts
@@ -44,6 +44,8 @@ export interface InitializeOptions {
     gaOptions?: GaOptions;
     alwaysSendToDefaultTracker?: boolean;
     standardImplementation?: boolean;
+    /** Optional. Defaults to `true`. Enables redacting a email as the string that in "Event Category" and "Event Action". */
+    redactEmail?: boolean;
 }
 
 export type Tracker = {
